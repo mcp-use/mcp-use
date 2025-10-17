57000ee8fe8e0d2422d6ae4727f4ad6a70ca30d2
pietrozullo <pietro.zullo@gmail.com>
2025-10-17 10:27:53 +0200

    move: task_managers -> client.task_managers

--- libraries/python/mcp_use/task_managers/__init__.py
+++ libraries/python/mcp_use/task_managers/__init__.py
@@ -1,20 +1,38 @@
-"""
-Connectors for various MCP transports.
+# mcp_use/task_managers/__init__.py
+from typing_extensions import deprecated
 
-This module provides interfaces for connecting to MCP implementations
-through different transport mechanisms.
-"""
+from mcp_use.client.task_managers import (
+    ConnectionManager as _ConnectionManager,
+)
+from mcp_use.client.task_managers import (
+    SseConnectionManager as _SseConnectionManager,
+)
+from mcp_use.client.task_managers import (
+    StdioConnectionManager as _StdioConnectionManager,
+)
+from mcp_use.client.task_managers import (
+    StreamableHttpConnectionManager as _StreamableHttpConnectionManager,
+)
+from mcp_use.client.task_managers import (
+    WebSocketConnectionManager as _WebSocketConnectionManager,
+)
 
-from .base import ConnectionManager
-from .sse import SseConnectionManager
-from .stdio import StdioConnectionManager
-from .streamable_http import StreamableHttpConnectionManager
-from .websocket import WebSocketConnectionManager
 
-__all__ = [
-    "ConnectionManager",
-    "StdioConnectionManager",
-    "WebSocketConnectionManager",
-    "SseConnectionManager",
-    "StreamableHttpConnectionManager",
-]
+@deprecated("Use mcp_use.client.task_managers.ConnectionManager")
+class ConnectionManager(_ConnectionManager): ...
+
+
+@deprecated("Use mcp_use.client.task_managers.StdioConnectionManager")
+class StdioConnectionManager(_StdioConnectionManager): ...
+
+
+@deprecated("Use mcp_use.client.task_managers.WebSocketConnectionManager")
+class WebSocketConnectionManager(_WebSocketConnectionManager): ...
+
+
+@deprecated("Use mcp_use.client.task_managers.SseConnectionManager")
+class SseConnectionManager(_SseConnectionManager): ...
+
+
+@deprecated("Use mcp_use.client.task_managers.StreamableHttpConnectionManager")
+class StreamableHttpConnectionManager(_StreamableHttpConnectionManager): ...
