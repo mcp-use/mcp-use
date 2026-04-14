c8b167b7601902628cb826accee3e0303ff42232
Andrew Khadder <andrew.khadder@gmail.com>
2026-04-14 14:50:09 -0700

    feat(inspector): pass elicitation handlers from ChatTab to MessageList

--- libraries/typescript/packages/inspector/src/client/components/ChatTab.tsx
+++ libraries/typescript/packages/inspector/src/client/components/ChatTab.tsx
@@ -884,6 +884,9 @@ export function ChatTab({
             tools={connection.tools}
             sendMessage={(msg, atts) => sendMessage(msg, [], atts)}
             serverBaseUrl={connection.url}
+            pendingElicitationRequests={connection.pendingElicitationRequests}
+            onApproveElicitation={connection.approveElicitation}
+            onRejectElicitation={connection.rejectElicitation}
           />
         )}
       </div>
